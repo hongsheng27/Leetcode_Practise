03c03142bafad6da10f2e978fd0cef81f165b115
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-09 18:27:10 +0800

    Time: 25 ms (8.61%), Space: 12.4 MB (86.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        for line in board:
+            count = {}
+            for c in line:
+                if c == ".":
+                    continue
+                count[c] = 1 + count.get(c, 0)
+                if count[c] > 1 : return False
+        # print('pass 1')
+        seen = {}
+        for i in range(9):
+            for line in board:
+                if line[i] == '.':
+                    continue
+                seen[line[i]] = 1 + seen.get(line[i], 0)
+                if seen[line[i]] > 1:
+                    return False
+            seen = {}
+
+        # print('pass 2')
+        for box_row in range(0,9,3):
+            for box_col in range(0,9,3):
+    	        count = {}
+                for i in range(3):
+                    for j in range(3):
+                        num = board[box_row+i][box_col+j]
+                        if num != ".":
+                            count[num] = 1 + count.get(num, 0)
+                        if count.get(num) > 1:
+                            return False
+                print(count)
+                count = {}
+                    
+        return True
+
+        
