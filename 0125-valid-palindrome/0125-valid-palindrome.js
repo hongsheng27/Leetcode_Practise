7fcdc5e19a6349f877d86ff64eb364cb62382f1c
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-10 16:23:46 +0800

    Time: 4 ms (75.85%), Space: 56.5 MB (80.94%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,15 @@
+var isPalindrome = function (s) {
+  let newStr = s.toLowerCase().replace(/[^a-z0-9]+/g, "");
+  let left = 0;
+  let right = newStr.length - 1;
+
+  while (right >= left) {
+    if (newStr[left] === newStr[right]) {
+      left++;
+      right--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
