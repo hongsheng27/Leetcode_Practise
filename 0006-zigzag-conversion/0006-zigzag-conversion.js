8220e4df013de31aab8e9c05b894a4020978742b
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-10 16:33:59 +0800

    Time: 9 ms (40.96%), Space: 58 MB (82.75%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows > s.length || numRows === 1) return s;
+    const arrays = Array.from({length: numRows}, ()=>'')
+    let index = 0
+    let direction = 1
+     
+    for(const elem of s){
+        arrays[index]+=elem
+
+        index += direction
+
+        if( index === numRows - 1 || index === 0) {    
+                direction = -direction 
+        }
+    }
+    
+    return arrays.join('')
+};
