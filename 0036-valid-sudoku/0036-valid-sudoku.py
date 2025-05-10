1ae83eee240ff87b6dbe9e9f70de987532a1847f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-10 21:50:37 +0800

    Time: 12 ms (28.05%), Space: 12.3 MB (86.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,35 +1,21 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        # check row
-        for row in board:
-            seen = set()
-            for val in row:
-                if val != ".":
-                    if val in seen:
-                        return False
-                    seen.add(val)
-  
-        # check column
-        for col in range(9):
-            seen = set()
-            for row in board:
-                val = row[col]
-                if val != '.':
-                    if val in seen:
-                        return False
-                    seen.add(val)
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        # check 3*3 box
-        for box_row in range(0,9,3):
-            for box_col in range(0,9,3):
-    	        seen = set()
-                for r in range(3):
-                    for c in range(3):
-                        val = board[box_row + r][box_col + c]
-                        if val != ".":
-                            if val in seen: 
-                                return False
-                            seen.add(val)     
+        for row in range(9):
+            for col in range(9):
+                val = board[row][col]
+                if val == '.':
+                    continue
+                if (val in rows[row] or val in cols[col] or val in squares[(col // 3, row // 3)]):
+                    return False
+                rows[row].add(val)
+                cols[col].add(val)
+                squares[(col // 3, row // 3)].add(val)
+
+
         return True
 
         
