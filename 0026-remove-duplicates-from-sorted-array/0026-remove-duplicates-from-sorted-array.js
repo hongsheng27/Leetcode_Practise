f45f91391639e153fae3e37b51586d5351bf1d3d
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-10 16:29:18 +0800

    Time: 20 ms (14.45%), Space: 60.5 MB (5.04%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  console.log(nums, i);
+  return i + 1;
+};
