54bdcb0b078ce7fa94c03d1d91cb9f1a1630843c
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-11 11:46:36 +0800

    Time: 39 ms (29.22%), Space: 12.4 MB (99.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,20 @@
 class Solution(object):
     def isPalindrome(self, s):
-        string = re.sub(r'[^A-Za-z0-9]', '', s).lower()
-        left, right = 0, len(string) - 1
+        left, right = 0, len(s) - 1
         
         while left < right:
-            if string[left] != string[right]:
+            while left < right and not self.isAlphaNumeric(s[left]):
+                left += 1
+            while left < right and not self.isAlphaNumeric(s[right]):
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
+
             left += 1
             right -= 1       
 
         return True
+    def isAlphaNumeric(self, c):
+        return (ord("A") <= ord(c) <= ord("Z") or 
+                ord("a") <= ord(c) <= ord("z") or 
+                ord("0") <= ord(c) <= ord("9"))
