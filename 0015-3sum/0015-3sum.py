b0f19665cc9faf005c8d881deb0272f789a7f3fd
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-12 15:34:08 +0800

    Time: 862 ms (35.22%), Space: 18.5 MB (29.44%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -16,9 +16,11 @@ class Solution(object):
                 else:
                     res.append([nums[i], nums[left], nums[right]])
                     left += 1
+                    right -= 1
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
-
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
         return res
 
 
