834c018661c650af48ef1e56a83918ca2a0c5e62
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-20 09:39:24 +0800

    Time: 97 ms (5.05%), Space: 18.1 MB (21.27%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1] * len(nums1)
+        for index_1, c in enumerate(nums1):
+            currentNums = float('inf')
+            for index_2, j in enumerate(nums2):
+                if c == j:
+                    currentNums = j
+                elif j > currentNums:
+                    res[index_1] = j
+                    break
+        print(res)
+        return res
