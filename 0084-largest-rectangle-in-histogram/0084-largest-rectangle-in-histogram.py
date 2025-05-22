7bbc585f82075f5e8f280780ca1302a9c382baf7
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-22 22:43:37 +0800

    Time: 337 ms (5.05%), Space: 50.5 MB (5.41%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -16,7 +16,6 @@ class Solution:
                 [index, height] = stack2.pop()
                 leftmost_lst[index] = i + 1
             stack2.append([i, heights[i]])
-        print(leftmost_lst, rightmost_lst)
         for i in range(n):
             height = heights[i]
             width = rightmost_lst[i] - leftmost_lst[i] + 1
