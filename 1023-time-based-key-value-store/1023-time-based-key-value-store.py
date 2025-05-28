2e9900b258e437bdb290968ae5cc463ff7398b23
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-05-28 14:31:06 +0800

    Time: 112 ms (68.95%), Space: 75.6 MB (13%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,33 +1,27 @@
 class TimeMap:
     def __init__(self):
-        self.storage = {}
+        self.store = {}
         
     def set(self, key: str, value: str, timestamp: int) -> None:
-        res = (timestamp, value)
-        if key not in self.storage:
-            self.storage[key] = []
-        self.storage[key].append(res)
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([timestamp, value])
         
 
     def get(self, key: str, timestamp: int) -> str:
-        if key in self.storage:
-            l, r = 0, len(self.storage[key]) - 1
+        if key in self.store:
+            values = self.store.get(key, [])
+            l, r = 0, len(values) - 1
             
             while l <= r:
                 m = (l + r) // 2
-                if self.storage[key][m][0] < timestamp:
+                if self.store[key][m][0] < timestamp:
                     l = m + 1
-                elif self.storage[key][m][0] > timestamp:
+                elif self.store[key][m][0] > timestamp:
                     r = m - 1
                 else:
-                    return self.storage[key][m][1]
-            return self.storage[key][r][1] if 0 <= r <= len(self.storage[key]) - 1 else ""
+                    return self.store[key][m][1]
+            # if no found, r is the closest index to the answer
+            return self.store[key][r][1] if 0 <= r <= len(self.store[key]) - 1 else ""
         else:
             return ""
-        
-
-
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
