475c94910a3a441e0a7c5ab62bdf21a2dc919d7a
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-06-05 14:15:08 +0800

    Time: 12 ms (90.95%), Space: 17.9 MB (62.53%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,10 +3,9 @@ class Solution:
         if len(s1) > len(s2): return False
         s1Count =  [0] * 26
         s2Count = [0] * 26
-        for i in s1:
-            s1Count[ord(i) - ord('a')] += 1 
-        for j in s2[:len(s1)]:
-            s2Count[ord(j) - ord('a')] += 1 
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1 
+            s2Count[ord(s2[i]) - ord('a')] += 1 
         l = 0
         if s1Count == s2Count:
             return True
