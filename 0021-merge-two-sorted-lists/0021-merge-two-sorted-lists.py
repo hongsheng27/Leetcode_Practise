5c07e54e9b999baa2f244eaaf1c0cdb3da7b086f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-06-09 15:15:25 +0800

    Time: 0 ms (100%), Space: 17.7 MB (61.2%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,27 +5,15 @@ class Solution:
         tail = dummy
         
         while p1 and p2:  
-            if not p1:
-                tail.next = p2
-                break
-            elif not p2:
-                tail.next = p1
-                break
-
             if p1.val <= p2.val:
                 tail.next = p1
-                tail = p1
                 p1 = p1.next
             else:
                 tail.next = p2
-                tail = p2
                 p2 = p2.next
+            tail = tail.next
         
-        if p1:
-            tail.next = p1
-        else:
-            tail.next = p2
-            
+        tail.next = p1 if p1 else p2
         return dummy.next
         
         
