f5fa44bb89652b15e7a3659f2e9f3ae7d73668d9
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-06-11 00:07:45 +0800

    Time: 852 ms (34.04%), Space: 18.4 MB (40.85%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,19 +6,28 @@ class Solution(object):
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            left = i + 1
-            right = len(nums) - 1 
-            while left < right:
-                if nums[left] + nums[right] < -nums[i]:
-                    left += 1
-                elif nums[left] + nums[right] > -nums[i]:
-                    right -= 1
+            l, r = i + 1, len(nums) - 1 
+
+            while l < r:
+                if nums[l] + nums[r] < -nums[i]:
+                    l += 1
+                elif nums[l] + nums[r] > -nums[i]:
+                    r -= 1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[l] == nums[r + 1]:
+                        r -= 1
         return res
+
+
+                
+
+
+                
+
+
+        
