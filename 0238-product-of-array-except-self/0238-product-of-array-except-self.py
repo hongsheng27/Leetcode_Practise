df3c7df7d7ab1f91ebf0c50f4c8a433744893791
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-06-14 00:33:33 +0800

    Time: 35 ms (47.65%), Space: 22.5 MB (23.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,11 +5,14 @@ class Solution(object):
         pref = [1] * n
         suff = [1] * n
 
-        for i in range(1, n):
-            pref[i] = nums[i - 1] * pref[i - 1]
-        for i in range(n - 2, -1, -1):
-            suff[i] = nums[i + 1] * suff[i + 1]
-        print(pref, suff)
+        prefix = 1
+        for i in range(n):
+            pref[i] = prefix
+            prefix *= nums[i]
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            suff[i] = suffix
+            suffix *= nums[i]
         for i in range(n):
             res[i] = pref[i] * suff[i]
         return res
