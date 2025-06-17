9b948b50fb5a1c54931e4a57fb2ee8f9c8828479
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-06-17 10:27:21 +0800

    Time: 1147 ms (11.63%), Space: 20.1 MB (89.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,21 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        while len(lists) > 1:
-            list1, list2 = lists[0], lists[1]
-            dummy = tail = ListNode()
-            while list1 and list2:
-                if list1.val <= list2.val:
-                    tail.next = list1
-                    list1 = list1.next
-                else:
-                    tail.next = list2
-                    list2 = list2.next
-                tail = tail.next
-            tail.next = list1 or list2
-            lists[0] = dummy.next
-            lists.pop(1)
-        return lists[0] if len(lists) > 0 else None
+        if len(lists) == 0: return None
+        
+        for i in range(1, len(lists)):
+            lists[i] = self.mergeList(lists[i - 1], lists[i])
+        return lists[-1]
+    
+    def mergeList(self, list1, list2):
+        dummy = tail = ListNode()
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        tail.next = list1 or list2
+        return dummy.next
 
             
         
