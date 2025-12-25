64e49f5631b87887319530b0544080c23d427391
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-12-25 14:05:31 -0800

    Time: 35 ms (54.61%), Space: 19.5 MB (17.92%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        freq = Counter(hand)
+        hand.sort()
+        p = 0
+        while p < len(hand):
+            freq[hand[p]] -= 1
+            for i in range(1, groupSize):
+                if freq[hand[p] + i]:
+                    freq[hand[p] + i] -= 1
+                else: return False
+            p += 1
+            while p < len(hand) and not freq[hand[p]]:
+                p += 1
+        return True
+
