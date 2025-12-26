33006e5df6b9f0ef3341009a37ee0046f736c12b
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-12-25 18:39:14 -0800

    Time: 35 ms (54.61%), Space: 19.3 MB (22.36%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -3,15 +3,11 @@ class Solution:
         if len(hand) % groupSize: return False
         freq = Counter(hand)
         hand.sort()
-        p = 0
-        while p < len(hand):
-            freq[hand[p]] -= 1
-            for i in range(1, groupSize):
-                if freq[hand[p] + i]:
-                    freq[hand[p] + i] -= 1
+        for num in hand:
+            if not freq[num]: continue
+            for i in range(num, num + groupSize):
+                if freq[i]:
+                    freq[i] -= 1
                 else: return False
-            p += 1
-            while p < len(hand) and not freq[hand[p]]:
-                p += 1
         return True
 
