8e8aa684ada2a49aa45f8a150f4232aa54b2fb55
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2025-12-28 14:32:06 -0800

    Time: 11 ms (31.05%), Space: 20.3 MB (97.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,9 +6,7 @@ class Solution:
         
         for i in range(1, len(intervals)):
             if intervals[i][0] <= res[-1][1]:
-                interval = res.pop()
-                newInterval = [interval[0], max(interval[1], intervals[i][1])]
-                res.append(newInterval)
+                res[-1][1] = max(res[-1][1], intervals[i][1])
             else:
                 res.append(intervals[i])
         return res
