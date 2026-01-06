849a2a2cb1f8bbb5b8ff378996ba4a7bc514fc6f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-06 00:40:53 -0800

    Time: 0 ms (100%), Space: 19.6 MB (11.67%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,10 +1,10 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(i):
+        def backtrack(i):
             if i >= len(nums):
                 return [[]]
             nextPerm = []
-            perm = dfs(i + 1)
+            perm = backtrack(i + 1)
 
             for p in perm:
                 for j in range(len(p)+1):
@@ -12,7 +12,7 @@ class Solution:
                     copyP.insert(j, nums[i])
                     nextPerm.append(copyP)
             return nextPerm
-        return dfs(0)
+        return backtrack(0)
 
 
         
