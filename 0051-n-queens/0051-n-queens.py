b3c20ee5bda4c2241c01f5a9b9a77657ad096513
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-08 21:32:58 -0800

    Time: 11 ms (70.39%), Space: 20 MB (5.33%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,8 +1,8 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         col = set()
-        posDiag = set() # (r - c)
-        negDiag = set() # (r + c)
+        posDiag = set() # (r + c)
+        negDiag = set() # (r - c)
 
         res = []
         board = [['.'] * n for _ in range(n)]
@@ -12,17 +12,17 @@ class Solution:
                 res.append(copy)
                 return
             for c in range(n):
-                if c in col or r - c in posDiag or r + c in negDiag: continue
+                if c in col or r + c in posDiag or r - c in negDiag: continue
                 col.add(c)
-                posDiag.add(r - c)
-                negDiag.add(r + c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
                 board[r][c] = 'Q'
 
                 backtrack(r + 1)
 
                 col.remove(c)
-                posDiag.remove(r - c)
-                negDiag.remove(r + c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
                 board[r][c] = '.'
         
         backtrack(0)
