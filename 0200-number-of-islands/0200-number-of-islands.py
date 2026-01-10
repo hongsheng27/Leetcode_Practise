4fdf7523d7fb210ba405199efea188dd67fa7010
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-09 21:33:58 -0800

    Time: 233 ms (82.19%), Space: 21.7 MB (34.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,9 +4,9 @@ class Solution:
         count = 0
         visit = set()
         def dfs(r, c):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == "0" or (r, c) in visit: 
+            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == "0": 
                 return 
-            visit.add((r, c))
+            grid[r][c] = '0'
             dfs(r + 1, c)
             dfs(r - 1, c)
             dfs(r, c + 1)
@@ -14,7 +14,7 @@ class Solution:
 
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == "1" and (i, j) not in visit:
+                if grid[i][j] == '1':
                     dfs(i, j)
                     count += 1
         return count
