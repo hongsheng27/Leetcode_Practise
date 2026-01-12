f471c4d7a6432ac2a08036e3255eb9174e654481
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-12 15:37:52 -0800

    Time: 619 ms (5.15%), Space: 34 MB (5.28%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROWS, COLS = len(board), len(board[0])
+        def dfs(r, c, visit):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] == "X" or (r, c) in visit: return
+            visit.add((r, c))
+            dfs(r + 1, c, visit)
+            dfs(r - 1, c, visit)
+            dfs(r, c + 1, visit)
+            dfs(r, c - 1, visit)
+            return visit
+        def isSurround(connect):
+            neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for r, c in connect:
+                for dr, dc in neighbors:
+                    nr, nc = r + dr, c + dc
+                    if nr < 0 or nc < 0 or nr == ROWS or nc == COLS:
+                        return False
+            return True
+        def fillX(visit):
+            for r, c in visit:
+                board[r][c] = "X"
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O": 
+                    connect = dfs(r, c, set())
+                    if isSurround(connect): fillX(connect)
