2716c349ab744134cd31d92fd78c9b7662b9391f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-12 15:56:40 -0800

    Time: 62 ms (8.11%), Space: 34 MB (5.28%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -16,7 +16,6 @@ class Solution:
         def isSurround(connect):
             neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             for r, c in connect:
-                visit.add((r, c))
                 for dr, dc in neighbors:
                     nr, nc = r + dr, c + dc
                     if nr < 0 or nc < 0 or nr == ROWS or nc == COLS:
@@ -31,4 +30,5 @@ class Solution:
                 if board[r][c] == "O" and (r, c) not in visit: 
                     connect = set()
                     dfs(r, c, connect)
+                    visit.update(connect)
                     if isSurround(connect): fillX(connect)
