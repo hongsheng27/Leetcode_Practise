98213b136bf842f289259c3b1b89b36dfc877ac5
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-16 12:45:52 -0800

    Time: 0 ms (100%), Space: 19.7 MB (14.52%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(edges)
         for i in range(1, n + 1):
             par[i] = i
-            rank[i] = 0
+            rank[i] = 1
 
         def find(n):
             p = par[n]
@@ -19,11 +19,10 @@ class Solution:
 
             if rank[p1] > rank[p2]:
                 par[p2] = p1
-            elif rank[p1] < rank[p2]:
-                par[p1] = p2
+                rank[p1] += rank[p2]
             else:
-                par[p2] = p1
-                rank[p1] += 1
+                par[p1] = p2
+                rank[p2] += rank[p1]
             return True
             
         for u, v in edges:
