fa522b4e15a4407bd04844de5b7cab8da07527b0
will-neu <liu.hun@northeastern.edu>
2026-01-26 16:51:53 -0800

    feat: upload 322 2d

---
+++ 0322-coin-change/0322-coin-change-memoization-2d.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        N = len(coins)
+        cache = [[-1] * (amount + 1) for _ in range(N)]
+        def dfs(i, total):
+            if total == amount:
+                return 0
+            if total > amount or i >= len(coins):
+                return float('inf')
+            if cache[i][total] != -1: return cache[i][total]
+            # skip
+            res1 = dfs(i + 1, total)
+            # not skip
+            res2 = 1 + dfs(i, total + coins[i])
+            cache[i][total] =  min(res1, res2)
+            return cache[i][total]
+        result = dfs(0, 0)
+        return result if result != float('inf') else -1
