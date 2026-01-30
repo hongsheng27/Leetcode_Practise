a0531e10abf39facddfa6145e452db608b73d2f3
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-01-30 13:20:25 -0800

    Time: 1315 ms (44.23%), Space: 19.4 MB (41.21%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,13 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        cache = {len(nums): 1}
-        def dfs(i):
-            if i in cache: return cache[i]
-            res = 1
+        dp = [-1] * (len(nums) + 1)
+        dp[-1] = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            maxVlue = 1
             for j in range(i + 1, len(nums)):
-                if nums[j] > nums[i]:
-                    res = max(res, 1 + dfs(j))
-            cache[i] = res
-            return res
-        return max(dfs(i) for i in range(len(nums)))
+                include = 1
+                if nums[i] < nums[j]:
+                    include += dp[j]
+                    maxVlue = max(maxVlue, include)
+            dp[i] = maxVlue
+        return max(dp[i] for i in range(len(nums)))
         
