3d89869e1e0d3f047e3c83ae1f2d8637fe1d9e24
will-neu <liu.hun@northeastern.edu>
2026-02-13 21:47:38 -0800

    feat: upload 79 visited

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -5,6 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # postorder solution
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root: return None
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
