f14e9285db8b9a5d258779d00e1d8ab816788a65
will-neu <liu.hun@northeastern.edu>
2026-02-15 18:52:22 -0800

    feawt: upload 332 backtrack

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,15 +1,28 @@
+
+#  can't not pass in leetcode
+# since pop(i) / insert(i, v) will cause TLE
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj = defaultdict(list)
-        for src, dst in sorted(tickets)[::-1]:
+        adj = {src: [] for src, dst in tickets}
+        tickets.sort()
+        for src, dst in tickets:
             adj[src].append(dst)
 
-        res = []
+        res = ["JFK"]
         def dfs(src):
-            while adj[src]:
-                dst = adj[src].pop()
-                dfs(dst)
-            res.append(src)
+            if len(res) == len(tickets) + 1:
+                return True
+            if src not in adj:
+                return False
 
-        dfs('JFK')
-        return res[::-1]
+            temp = list(adj[src])
+            for i, v in enumerate(temp):
+                adj[src].pop(i)
+                res.append(v)
+                if dfs(v): return True
+                adj[src].insert(i, v)
+                res.pop()
+            return False
+
+        dfs("JFK")
+        return res
