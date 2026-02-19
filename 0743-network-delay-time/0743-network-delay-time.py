5fd89157c394b1fa80a5406db31fe1b4d81be6f2
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-02-18 18:20:08 -0800

    Time: 356 ms (42.19%), Space: 21.6 MB (18.76%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,9 +1,9 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj = {}
-        for src, dst, w in times:
-            adj[src] = []
-            adj[dst] = []
+        for i in range(1, n + 1):
+            adj[i] = []
+
         for src, dst, w in times:
             adj[src].append((w, dst)) # [(distance, dst)]
 
