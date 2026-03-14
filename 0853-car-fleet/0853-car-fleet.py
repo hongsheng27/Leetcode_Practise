858b4efe6798190c88a22c9897506449994d6586
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-03-14 01:34:12 -0700

    Time: 544 ms (5.01%), Space: 46.7 MB (5.09%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleets = [] # (pos, time)
+        for i in range(len(position)):
+            fleets.append((position[i], (target - position[i]) / speed[i] ))
+
+        fleets.sort()
+        print(fleets)
+        stack = []
+    
+        for i in range(len(fleets) - 1, -1, -1):
+            if stack and stack[-1] >= fleets[i][1]:
+                continue
+            stack.append(fleets[i][1])
+        return len(stack)
+
+
