0cac2f03972408444d9b029b09ac978bb6264ca6
will-neu <liu.hun@northeastern.edu>
2026-03-15 18:55:07 -0700

    feat: uplaod 424 slinding window optimal

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement-sliding-window-optimal.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement-sliding-window-optimal.py
@@ -1,17 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        max_len = 0
+        max_freq = 0
+        l = 0
         count = {}
-        l = maxLen = 0
-        
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            while (r - l + 1) - max(count.values()) > k:
+            max_freq = max(max_freq, count[s[r]])
+            while (r - l + 1) - max_freq > k:
                 count[s[l]] -= 1
                 l += 1
-            maxLen = max(maxLen, r - l + 1)
-        return maxLen
-            
-
-        
-
-        
+            max_len = max(max_len, r - l + 1) 
+        return max_len
