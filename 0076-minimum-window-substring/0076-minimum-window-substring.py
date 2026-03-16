ffb8bbb1b49525dbff88e516143972256611c1e0
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-03-16 16:44:40 -0700

    Time: 129 ms (28.66%), Space: 19.6 MB (97.95%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        count = {}
+        for c in t:
+            count[c] = count.get(c, 0) + 1
+        l = have = 0
+        ans = []
+        for r in range(len(s)):
+            if s[r] in count:
+                count[s[r]] -= 1
+                if count[s[r]] == 0:
+                    have += 1
+            while have == len(count.keys()):
+                if not ans or r - l + 1 < len(ans):
+                    ans = s[l: r + 1]
+                if s[l] in count:
+                    count[s[l]] += 1
+                    if count[s[l]] == 1:
+                        have -= 1
+                l += 1
+        return "".join(ans)
+            
+                
+
+
+        
