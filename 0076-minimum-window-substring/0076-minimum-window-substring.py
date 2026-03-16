7dc42594f1c151d20eb19f149c66364c242b6dd7
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-03-16 16:56:11 -0700

    Time: 63 ms (75.29%), Space: 19.6 MB (97.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,21 +5,21 @@ class Solution:
         for c in t:
             count[c] = count.get(c, 0) + 1
         l = have = 0
-        ans = ""
+        ans = None
         for r in range(len(s)):
             if s[r] in count:
                 count[s[r]] -= 1
                 if count[s[r]] == 0:
                     have += 1
             while have == len(count):
-                if not ans or r - l + 1 < len(ans):
-                    ans = s[l: r + 1]
+                if not ans or r - l < ans[1] - ans[0]:
+                    ans = (l, r)
                 if s[l] in count:
                     count[s[l]] += 1
                     if count[s[l]] == 1:
                         have -= 1
                 l += 1
-        return ans
+        return "" if not ans else s[ans[0]: ans[1] + 1] 
             
                 
 
