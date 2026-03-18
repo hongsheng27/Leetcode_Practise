045ec5c77163de98518513e00246454a1ac7204f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-03-18 01:06:18 -0700

    Time: 183 ms (69.65%), Space: 25.9 MB (76.6%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -6,13 +6,13 @@ class Solution:
         for r in range(len(fruits)):
             count[fruits[r]] = count.get(fruits[r], 0) + 1
             
-            while len(count) == 3:
+            while len(count) > 2:
                 count[fruits[l]] -= 1
                 if not count[fruits[l]]:
                     del count[fruits[l]]
                 l += 1
             res = max(res, r - l + 1)
-        return res if res else r - l + 1
+        return res
 
 
             
