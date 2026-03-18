fc4150e1809da3037b38a8bda64cf926055e687b
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-03-18 01:00:04 -0700

    Time: 195 ms (40.04%), Space: 26.1 MB (10.56%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,22 +1,21 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         count = {}
-        have = l = res = 0 
+        l = res = 0 
 
         for r in range(len(fruits)):
             if fruits[r] not in count or not count[fruits[r]]:
                 count[fruits[r]] = 1
-                have += 1
             else:
                 count[fruits[r]] += 1
             
-            while have == 3:
+            while len(count) == 3:
                 count[fruits[l]] -= 1
-                if count[fruits[l]] == 0:
-                    have -= 1  
+                if not count[fruits[l]]:
+                    del count[fruits[l]]
                 l += 1
             res = max(res, r - l + 1)
-        return res if res else sum(count.values())
+        return res if res else r - l + 1
 
 
             
