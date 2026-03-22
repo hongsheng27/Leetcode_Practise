24b1b524051d81d2b6b272b7230c9246e4f99a3c
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-03-22 12:00:19 -0700

    Time: 82 ms (57.25%), Space: 21.6 MB (70.37%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -1,19 +1,19 @@
 class TicTacToe:
     def __init__(self, n: int):
+        self.n = n
         self.h = [0] * n
         self.v = [0] * n
         self.d = 0
         self.rd = 0
     def move(self, row: int, col: int, player: int) -> int:
         number = 1 if player == 1 else -1
-        n = len(self.h)
         self.h[row] += number 
         self.v[col] += number
         if row - col == 0:
             self.d += number
-        if row + col == n - 1:
+        if row + col == self.n - 1:
             self.rd += number
-        if abs(self.h[row]) == n or abs(self.v[col]) == n or abs(self.d) == n or abs(self.rd) == n:
+        if abs(self.h[row]) == self.n or abs(self.v[col]) == self.n or abs(self.d) == self.n or abs(self.rd) == self.n:
             return player
         return 0
 
