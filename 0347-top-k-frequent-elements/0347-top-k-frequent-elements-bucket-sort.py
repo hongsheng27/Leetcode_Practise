2dcc698113f6e47141a28621352346aa2810f876
will-neu <liu.hun@northeastern.edu>
2026-03-31 17:02:04 -0700

    feat: upload 347 bucket sort

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements-bucket-sort.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements-bucket-sort.py
@@ -1,14 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
+        count = Counter(nums)
+        freq = [[] for _ in range(len(nums) + 1)] # include 0 bucket
+        res = []
+        
         for num, cnt in count.items():
             freq[cnt].append(num)
-        
-        res = []
-        for j in range(len(freq) - 1, 0 , -1):
-            for n in freq[j]:
-                res.append(n)
-                if(len(res) == k): return res    
+        for i in range(len(freq) - 1, 0, -1):
+            for j in freq[i]:
+                res.append(j)
+                k -= 1
+                if k == 0: return res
