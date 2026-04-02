17b5ba3a0d622838193677605692d17c0eb763ac
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-04-01 19:06:05 -0700

    Time: 0 ms (100%), Space: 19.4 MB (52.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,6 +3,6 @@ class Solution:
         res = ""
         for i in range(len(strs[0])):
             for j in range(1, len(strs)):
-                if i > len(strs[j]) - 1 or strs[0][i] != strs[j][i]: return res
+                if i == len(strs[j]) or strs[0][i] != strs[j][i]: return res
             res += strs[0][i]
         return res
