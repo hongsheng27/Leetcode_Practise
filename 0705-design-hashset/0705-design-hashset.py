97450d6353ff76cfeccf06ccee63f553fac79997
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-04-04 00:13:35 -0700

    Time: 32 ms (69.81%), Space: 24.2 MB (89.33%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -2,27 +2,22 @@ class MyHashSet:
     def __init__(self):
         self.setList = [[] for _ in range(1000)]
     def add(self, key: int) -> None:
-        index = key % 1000
+        index = key % len(self.setList)
         for elem in self.setList[index]:
             if elem == key:
                 return
         self.setList[index].append(key)
     def remove(self, key: int) -> None:
-        index = key % 1000
+        index = key % len(self.setList)
         for i, elem in enumerate(self.setList[index]):
             if elem == key:
                 self.setList[index].pop(i)
     def contains(self, key: int) -> bool:
-        index = key % 1000
+        index = key % len(self.setList)
         for elem in self.setList[index]:
             if elem == key:
                 return True
         return False
-
-    
-
-        
-
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
