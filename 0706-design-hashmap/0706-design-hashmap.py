232e345743491ebd5ff0a74e420c09fd635cfefc
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-04-04 12:13:46 -0700

    Time: 58 ms (40.22%), Space: 22.9 MB (55.16%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,18 +1,32 @@
 class MyHashMap:
     def __init__(self):
-        self.mapList = [-1 for _ in range(10**6)]
+        self.mapList = [[] for _ in range(1009)]
 
     def put(self, key: int, value: int) -> None:
         index = key % len(self.mapList)
-        self.mapList[index] = value
+        for i, elem in enumerate(self.mapList[index]):
+            k, v = elem
+            if k == key:
+                self.mapList[index].pop(i)       
+        self.mapList[index].append((key, value))
 
     def get(self, key: int) -> int:
         index = key % len(self.mapList)
-        return self.mapList[index]
+        for i, elem in enumerate(self.mapList[index]):
+            k, v = elem
+            print(k, v)
+            if k == key:
+                return v
+        return -1
 
     def remove(self, key: int) -> None:
         index = key % len(self.mapList)
-        self.mapList[index] = -1
+        for i, elem in enumerate(self.mapList[index]):
+            k, v = elem
+            if k == key:
+                self.mapList[index].pop(i)
+
+
         
 
 
