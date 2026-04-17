2d16ee1160f10d255f48272c25faa45d7462e32b
will-neu <liu.hun@northeastern.edu>
2026-04-17 00:02:46 -0700

    feat: upload 42 two pointer

--- 0042-trapping-rain-water/0042-trapping-rain-water-two-pointer.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water-two-pointer.py
@@ -1,16 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        res = 0
-        l, r = 0, len(height) - 1
-        leftMax, rightMax = height[l], height[r]
+        l = leftMax = rightMax = 0
+        r = len(height) - 1
+        total = 0
         while l < r:
-            if height[l] <= height[r]:
-                leftMax = max(leftMax, height[l])
+            leftMax = max(leftMax, height[l])
+            rightMax = max(rightMax, height[r])
+            if leftMax <= rightMax:
                 l += 1
-                water = max(0, leftMax - height[l])
-            else:
-                rightMax = max(rightMax, height[r])
-                r -= 1
-                water = max(0, rightMax - height[r])
-            res += water
-        return res
+                total += max(0, leftMax - height[l])
+            else:   
+                r-= 1
+                total += max(0, rightMax - height[r])
+        return total
