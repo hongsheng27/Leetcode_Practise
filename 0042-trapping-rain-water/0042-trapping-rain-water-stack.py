e8dd1e5d390f9184f1faf12c70258aa44ddb05f7
will-neu <liu.hun@northeastern.edu>
2026-04-17 01:31:36 -0700

    feat: upload 42 monoitic stack

--- 0042-trapping-rain-water/0042-trapping-rain-water-stack.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water-stack.py
@@ -1,16 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         stack = []
-        res = 0
-        for i, h in enumerate(height):
-            while stack and stack[-1][1] <= h:
-                mid = stack.pop()[1]
+        res = 0 
+        for i in range(len(height)):
+            while stack and height[stack[-1]] < height[i]:
+                ground = height[stack.pop()]
                 if stack:
-                    left = stack[-1][1]
-                    right = h
-                    _h = min(left, right) - mid
-                    w = i - stack[-1][0] - 1
-                    res += _h * w 
-            stack.append((i, h))
+                    l = height[stack[-1]]
+                    r = height[i]
+                    h = max(0, min(l, r) - ground)
+                    w = i - stack[-1] - 1
+                    res += w * h
+            stack.append(i)
         return res
-        
