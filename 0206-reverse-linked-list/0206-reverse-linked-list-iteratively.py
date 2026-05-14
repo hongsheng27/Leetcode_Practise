dcbb394f53b6acad607c3c0a1c39ebe6e26507df
will-neu <liu.hun@northeastern.edu>
2026-05-14 12:13:40 -0700

    feat: update iteratively

--- 0206-reverse-linked-list/0206-reverse-linked-list-iteratively.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list-iteratively.py
@@ -1,10 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, cur = None, head
+        pre = None
+        cur = head
+
         while cur:
             nxt = cur.next
-            cur.next = prev
-            prev = cur
+            cur.next = pre
+            pre = cur
             cur = nxt
-        return prev
+        return pre
+
         
