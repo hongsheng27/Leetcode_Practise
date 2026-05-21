181093922f2fd8cc1c2774f02c2044c06bac1d0e
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-05-21 01:51:18 -0700

    Time: 8 ms (69.99%), Space: 23.2 MB (17.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,25 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists: return None
-        while len(lists) > 1:
-            merged = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                merged.append(self.mergeTwo(l1, l2))
-            lists = merged
-        return lists[0]
-    def mergeTwo(self, node1, node2):
-        dummy = tail = ListNode()
-        while node1 and node2:
-            if node1.val <= node2.val:
-                tail.next = node1
-                node1 = node1.next
-            else:
-                tail.next = node2
-                node2 = node2.next
+        res = tail = ListNode(0)
+
+        minHeap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                minHeap.append((lists[i].val, i, lists[i]))
+        heapq.heapify(minHeap)
+
+        while minHeap:
+            [val, index, node] = heapq.heappop(minHeap)
+            tail.next = node
             tail = tail.next
-        tail.next = node1 or node2
-        return dummy.next
-        
+            newNode = node.next
+            if newNode:
+                heapq.heappush(minHeap, (newNode.val, index, newNode))
+        return res.next
+
+
+
+
+
+    
