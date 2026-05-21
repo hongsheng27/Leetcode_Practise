936f17951e0b39384d0a3a9b0b14b866ba512931
will-neu <liu.hun@northeastern.edu>
2026-05-21 22:27:41 +0800

    feat: upload 76 optimal

--- 0076-minimum-window-substring/0076-minimum-window-substring-sliding-window-optimal.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring-sliding-window-optimal.py
@@ -1,29 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): return ""
-
+        if len(t) > len(s): return ""
         count = {}
         for c in t:
             count[c] = count.get(c, 0) + 1
-
+        
         l = have = 0
-        ans = (0, float('inf'))
+        minRange = (0, float('inf'))
         for r in range(len(s)):
             if s[r] in count:
                 count[s[r]] -= 1
-                if count[s[r]] == 0:
-                    have += 1
+                if count[s[r]] == 0: have += 1
             while have == len(count):
-                if r - l < ans[1] - ans[0]:
-                    ans = (l, r)
+                if r - l < minRange[1] - minRange[0]:
+                    minRange = (l, r)
                 if s[l] in count:
                     count[s[l]] += 1
-                    if count[s[l]] == 1:
-                        have -= 1
+                    if count[s[l]] == 1: have -= 1
                 l += 1
-        return "" if ans[1] == float('inf') else s[ans[0]: ans[1] + 1] 
+        
+        return s[minRange[0]: minRange[1] + 1] if minRange[1] != float('inf') else ""
             
-                
-
-
+            
         
