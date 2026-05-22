6104e4babe4325adcf5f8dfcd88b38e7c52f9783
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-05-22 02:37:45 -0700

    Time: 0 ms (100%), Space: 19.3 MB (39.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,17 +6,21 @@ class Solution:
         p1 = m - 1
         p2 = n - 1
         p = m + n - 1
+
         while p2 >= 0:
-            num1 = nums1[p1] if p1 >= 0 else float('-inf')
-            num2 = nums2[p2]
-            if num2 > num1:
-                nums1[p] = num2
-                p2 -= 1
-            else:
-                nums1[p] = num1
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
                 p1 -= 1
-            p -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1 
+        
+        
+        
 
 
+        
+        
 
         
