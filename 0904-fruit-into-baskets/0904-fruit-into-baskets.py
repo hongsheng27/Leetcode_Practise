e552206cca1640b02a689fef55d25160b6728491
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-05-21 21:04:55 -0700

    Time: 186 ms (61.58%), Space: 26 MB (33.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -4,12 +4,11 @@ class Solution:
         l = 0
         maxNum = 0
         for r in range(len(fruits)):
-            while len(count) == 2 and fruits[r] not in count:
-                maxNum = max(maxNum, r - l)
+            count[fruits[r]] = count.get(fruits[r], 0) + 1
+            while len(count) > 2:
                 count[fruits[l]] -= 1
                 if not count[fruits[l]]: del count[fruits[l]]
-                l += 1
-            count[fruits[r]] = count.get(fruits[r], 0) + 1
-        maxNum = max(maxNum, r - l + 1)
+                l += 1  
+            maxNum = max(maxNum, r - l + 1)
         return maxNum
         
