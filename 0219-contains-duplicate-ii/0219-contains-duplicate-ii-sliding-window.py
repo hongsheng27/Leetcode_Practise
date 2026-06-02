93e8d2ff5752b5fe598dbb411676ffaadc9c58db
will-neu <liu.hun@northeastern.edu>
2026-06-02 18:12:12 +0800

    feat: upload 219 sliding window

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii-sliding-window.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii-sliding-window.py
@@ -1,9 +1,14 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        lastIndex = {} 
-        for i, n in enumerate(nums):
-            if n in lastIndex and i - lastIndex[n] <= k:
+        window = set()
+        l = 0
+
+        for r in range(len(nums)):
+            if r - l > k:
+                window.remove(nums[l])
+                l += 1
+            if nums[r] in window:
                 return True
-            lastIndex[n] = i
+            window.add(nums[r])
         return False
-                    
+        
