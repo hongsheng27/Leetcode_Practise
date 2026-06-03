023c4e36fedc0147ee8a5041f50f7b3e96b31897
will-neu <liu.hun@northeastern.edu>
2026-06-03 12:13:58 +0800

    chore: add prefix bs

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays-bs.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays-bs.py
@@ -1,17 +1,29 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m = len(nums1)
-        n = len(nums2)
-        i = j = 0
-        cur = prev = 0
-        for _ in range((m + n) // 2 + 1):
-            prev = cur
-            if i < m and (j >= n or nums1[i] <= nums2[j]):
-                cur = nums1[i]
-                i += 1
+        if len(nums1) > len(nums2): nums1, nums2 = nums2, nums1
+
+        n = len(nums1)
+        m = len(nums2)
+
+        l = 0
+        r = n
+
+        while l <= r:
+            i = (l + r) // 2
+            j = (m + n + 1) // 2 - i
+
+            nums1Left = nums1[i - 1] if i > 0 else float('-inf')
+            nums1Right = nums1[i] if i < n else float('inf')
+            nums2Left = nums2[j - 1] if j > 0 else float('-inf')
+            nums2Right = nums2[j] if j < m else float('inf')
+
+            if nums1Left <= nums2Right and nums2Left <= nums1Right:
+                if not (m + n) % 2:
+                    return (max(nums1Left, nums2Left) + min(nums1Right, nums2Right)) / 2
+                return max(nums1Left, nums2Left)
+            elif nums1Left > nums2Right:
+                r = i - 1
             else:
-                cur = nums2[j]
-                j += 1
-        if (m + n) % 2:
-            return cur
-        return (prev + cur) / 2
+                l = i + 1
+
+        
