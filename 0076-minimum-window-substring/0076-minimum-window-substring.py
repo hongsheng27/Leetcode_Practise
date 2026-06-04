178dd2df4b19530b56b421aaa6db232fc55f973d
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-04 00:15:13 -0700

    Time: 56 ms (89.25%), Space: 19.8 MB (37.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,28 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s): return ""
-        l = 0
-        minRange = (0, float('inf')) # l, r
         count = {}
         for c in t:
             count[c] = count.get(c, 0) + 1
-        have = 0
+        
+        l = have = 0
+        minRange = (0, float('inf'))
         for r in range(len(s)):
-            if s[r] in count: 
+            if s[r] in count:
                 count[s[r]] -= 1
-                if count[s[r]] == 0:
-                    have += 1
+                if count[s[r]] == 0: have += 1
             while have == len(count):
                 if r - l < minRange[1] - minRange[0]:
                     minRange = (l, r)
                 if s[l] in count:
                     count[s[l]] += 1
-                    if count[s[l]] == 1:
-                        have -= 1
+                    if count[s[l]] == 1: have -= 1
                 l += 1
-        if minRange[1] == float('inf'): return "" 
-        return s[minRange[0]: minRange[1] + 1]
-
-
+        
+        return s[minRange[0]: minRange[1] + 1] if minRange[1] != float('inf') else ""
+            
+            
+        
 
         
