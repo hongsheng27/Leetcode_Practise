9da131d23ca47455ef2a707cb6a65a94f931d2da
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-06 02:29:39 -0700

    Time: 4 ms (31.51%), Space: 19.7 MB (28.05%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,13 +1,12 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        pathway = [] # (position, pplStatus)
-        for trip in trips:
-            numPassenger, frm, to = trip
-            pathway.append((frm, numPassenger))
-            pathway.append((to, -numPassenger))
-        pathway.sort()
-        total = 0
-        for p in pathway:
-            total += p[1]
-            if total > capacity: return False
+        events = [] # (position, passengerChange)
+        for passengers, start, end in trips:
+            events.append((start, passengers))
+            events.append((end, -passengers))
+        events.sort()
+        cur = 0
+        for e in events:
+            cur += e[1]
+            if cur > capacity: return False
         return True
