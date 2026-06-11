434beef270ae9d3c5ca3b41ba7bb146b7257f3fc
will-neu <liu.hun@northeastern.edu>
2026-06-11 12:51:10 +0800

    feat: upload 134 prefixSum

--- 0134-gas-station/0134-gas-station-greedy-prefixSum.py
+++ 0134-gas-station/0134-gas-station-greedy-prefixSum.py
@@ -1,13 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost): return -1
-        total = start = 0
+        total = 0
+        minTotal = float('inf')
+        minIndex = -1
         for i in range(len(gas)):
             total += gas[i] - cost[i]
-            if total < 0:
-                start = i + 1
-                total = 0
-        return start
+            if total < minTotal:
+                minTotal = total
+                minIndex = i
+        return minIndex + 1 if minIndex != len(gas) - 1 else 0
         
             
 
