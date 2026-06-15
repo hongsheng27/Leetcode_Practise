c98aacdc66117bba69d582d154139e8b58ce1a44
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-15 03:40:19 -0700

    Time: 179 ms (21.54%), Space: 20.9 MB (15.91%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -7,11 +7,11 @@ class Solution:
         time = 0
         while maxHeap or q:
             time += 1
-            if q and time == q[0][0]:
+            while q and time == q[0][0]:
                 _, c = q.popleft()
                 heapq.heappush(maxHeap, c)
             if maxHeap:
-                cnt = heapq.heappop(maxHeap)
-                if cnt + 1 < 0:
-                    q.append((time + n + 1, cnt + 1)) 
+                freq = heapq.heappop(maxHeap)
+                if freq + 1 < 0:
+                    q.append((time + n + 1, freq + 1)) 
         return time
