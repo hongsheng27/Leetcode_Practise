32b24395add5dfd630b4a83c7f95a12a60c2f78f
will-neu <liu.hun@northeastern.edu>
2026-06-16 14:20:21 +0800

    feat: upload 90 choose v

--- 0090-subsets-ii/0090-subsets-ii-choose-or-not-choose.py
+++ 0090-subsets-ii/0090-subsets-ii-choose-or-not-choose.py
@@ -2,16 +2,19 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        def backtrack(i, path):
-            res.append(path.copy())
-
-            for j in range(i, len(nums)):
-                if j > i and nums[j] == nums[j - 1]:
-                    continue
-                path.append(nums[j])
-                backtrack(j + 1, path)
-                path.pop()
-            
-        backtrack(0, [])
+        curSet = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(curSet.copy())
+                return
+            # decision to include nums[i]
+            curSet.append(nums[i])
+            dfs(i + 1)
+            # decision to NOT include nums[i]
+            curSet.pop()
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            dfs(i + 1)
+        dfs(0)
         return res
         
