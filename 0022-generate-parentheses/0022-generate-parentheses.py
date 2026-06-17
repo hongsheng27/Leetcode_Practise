a72d1fcccbf8b67f3e7789a20173446454bcf9ed
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-16 18:41:53 -0700

    Time: 0 ms (100%), Space: 19.4 MB (73.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.res = []
-        def backtrack(left, right, stack):
-            if left == right == n:
-                self.res.append("".join(stack))
+        res = []
+        def backtrack(leftNum, rightNum, path):
+            if leftNum == rightNum == n:
+                res.append("".join(path))
                 return
-            if left < n:
-                stack.append('(')
-                backtrack(left + 1, right, stack)
-                stack.pop()
-            if right < n and left > right:
-                stack.append(')')
-                backtrack(left, right + 1, stack)
-                stack.pop()
+            if leftNum < n:
+                path.append('(')
+                backtrack(leftNum + 1, rightNum, path)
+                path.pop()
+            if leftNum > rightNum:
+                path.append(')')
+                backtrack(leftNum, rightNum + 1, path)
+                path.pop()
         backtrack(0, 0, [])
-        return self.res
+        return res
         
