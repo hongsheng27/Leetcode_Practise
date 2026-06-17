850a200cb7dcf1975e312e09809ba4fcbec41d6d
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-16 20:23:43 -0700

    Time: 0 ms (100%), Space: 21.2 MB (98.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,6 +1,5 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not intervals: return [newInterval]
         res = []
         for i, interval in enumerate(intervals):
             # before newInterval
@@ -12,8 +11,7 @@ class Solution:
                 return res + intervals[i:]
             # overlap
             else:
-                newInterval[0] = min(newInterval[0], interval[0])
-                newInterval[1] = max(newInterval[1], interval[1])
+                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
         res.append(newInterval)
         return res
         
