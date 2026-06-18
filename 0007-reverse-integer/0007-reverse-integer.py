3737f317203c7080fe7d3bef6bc28213b509bee9
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-17 20:03:07 -0700

    Time: 48 ms (43%), Space: 19.4 MB (9%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,8 +3,11 @@ class Solution:
         res = 0
         while x:
             digit = int(math.fmod(x, 10))
-            x = int(x / 10) 
-            res = 10 * res + digit 
-        
-        if res < -(2 ** 31) or res > 2 ** 31 - 1: return 0
+            x = int(x / 10)
+            res = res * 10 + digit
+        if res > 2 ** 31 - 1 or res < -(2 ** 31):
+            return 0
         return res
+        
+
+        
