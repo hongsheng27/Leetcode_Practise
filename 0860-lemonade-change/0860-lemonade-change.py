8d9ef890c2eb823ced24410123684b48cd55ea3d
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-18 19:29:04 -0700

    Time: 4 ms (44.09%), Space: 23.5 MB (13.77%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,20 +1,19 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        change = [0, 0] # five, ten 
+        fiveCount = tenCount = 0
         for bill in bills:
             if bill == 5:
-                change[0] += 1
+                fiveCount += 1
             elif bill == 10:
-                change[1] += 1
-                change[0] -= 1
+                tenCount += 1
+                fiveCount -= 1
             else:
-                if change[1] > 0 and change[0] > 0:
-                    change[1] -= 1
-                    change[0] -= 1
+                if tenCount > 0 and fiveCount > 0:
+                    tenCount -= 1
+                    fiveCount -= 1
                 else:
-                    change[0] -= 3
-            print(change)
-            if change[0] < 0 or change[1] < 0: return False
+                    fiveCount -= 3
+            if fiveCount < 0 or tenCount < 0: return False
         return True
         
                     
