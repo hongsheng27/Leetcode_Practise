c6d3e826c762380162ccd71386215d23410c6ecf
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-22 20:19:49 -0700

    Time: 3 ms (85.92%), Space: 19.2 MB (62.85%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,15 +1,15 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        lastIndexMap = {}
-        for i, c in enumerate(s):
-            lastIndexMap[c] = i
-        l = r = 0
         res = []
-        goal = lastIndexMap[s[0]]
-        while r < len(s):
-            goal = max(goal, lastIndexMap[s[r]])
-            if r == goal:
-                res.append(r - l + 1)
-                l = r + 1
-            r += 1
+        lastIndex = {}
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+
+        size = end = 0
+        for i, c in enumerate(s):
+            end = max(end, lastIndex[c])
+            size += 1
+            if i == end:
+                res.append(size)
+                size = 0
         return res
