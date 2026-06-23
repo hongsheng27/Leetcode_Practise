00e0c15a120b413e7953ef103ffa4e8131cf6d2b
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-22 20:41:04 -0700

    Time: 0 ms (100%), Space: 19.6 MB (22.47%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -3,15 +3,18 @@ class Solution:
         nums.sort()
         res = []
         def backtrack(i, path):
-            res.append(path.copy())
-
-            for j in range(i, len(nums)):
-                if j > i and nums[j] == nums[j - 1]:
-                    continue
-                path.append(nums[j])
-                backtrack(j + 1, path)
-                path.pop()
+            # base case
+            if i == len(nums):
+                res.append(path.copy())
+                return
+            # recursion case
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
             
+            while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+                i += 1
+            backtrack(i + 1, path)
+
         backtrack(0, [])
         return res
-        
