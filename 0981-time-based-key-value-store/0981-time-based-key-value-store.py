7af99987f7b4a946457fdf2f003faffe166b4fb0
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-23 22:05:32 -0700

    Time: 121 ms (64.57%), Space: 69 MB (67.8%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,26 +1,25 @@
 class TimeMap:
     def __init__(self):
-        self.count = {}
+        self.count = defaultdict(list) # (teimstamp, value)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.count:
-            self.count[key] = [(timestamp, value)]
-        else:
-            self.count[key].append((timestamp, value))
+        self.count[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.count: return ""
-        lst = self.count[key]
+        values = self.count[key]
         l = 0
-        r = len(lst) - 1
+        r = len(values) - 1
+        maxItem = (float('-inf'), "")
         while l <= r:
             m = (l + r) // 2
-            if lst[m][0] == timestamp: return lst[m][1]
-            if lst[m][0] < timestamp: 
+            if timestamp >= values[m][0]:
+                maxItem = values[m]
                 l = m + 1
             else:
                 r = m - 1
-        return lst[r][1] if r >= 0 and lst[r] else ""
+        return maxItem[1]
+
 
 
 # Your TimeMap object will be instantiated and called as such:
