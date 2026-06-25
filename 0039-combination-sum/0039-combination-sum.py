d5e6b130d497280695926261f0e1f856b6ee187e
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-25 00:09:30 -0700

    Time: 7 ms (75.92%), Space: 19.8 MB (8.7%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,15 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def backtrack(i, s, path):
-            if s == target:
+        def backtrack(i, total, path):
+            # base case
+            if total == target:
                 res.append(path.copy())
                 return
-            if i >= len(candidates) or s > target: return
+            if total > target or i >= len(candidates):
+                return
+            # recursion case
             for j in range(i, len(candidates)):
                 path.append(candidates[j])
-                backtrack(j, s + candidates[j], path)
+                backtrack(j, total + candidates[j], path)
                 path.pop()
-
+            
         backtrack(0, 0, [])
         return res
