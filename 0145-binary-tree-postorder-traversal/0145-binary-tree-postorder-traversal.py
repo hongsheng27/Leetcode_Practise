1acab4320417839020b73171fbf7d561e0dc4f8c
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-26 22:30:27 -0700

    Time: 0 ms (100%), Space: 19.3 MB (27.54%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -9,9 +9,10 @@ class Solution:
         res = []
         def dfs(node):
             if not node: return
-
+        
             dfs(node.left)
             dfs(node.right)
             res.append(node.val)
         dfs(root)
         return res
+            
