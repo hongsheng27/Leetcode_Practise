c2b3c762e11f087dd66c4881c824c474d0757bbd
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-27 23:15:06 -0700

    Time: 4348 ms (31.17%), Space: 19.6 MB (26.2%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROW, COL = len(board), len(board[0])
+        ROW = len(board)
+        COL = len(board[0])
         visited = set()
-        def dfs(r, c, index):
-            if index == len(word): return True
-            if (r < 0 or c < 0 or r >= ROW or c >= COL or
-                (r, c) in visited or board[r][c] != word[index]):
-                 return False
-            visited.add((r, c))
-            result = (dfs(r + 1, c, index + 1) or
-                      dfs(r - 1, c, index + 1) or
-                      dfs(r, c + 1, index + 1) or
-                      dfs(r, c - 1, index + 1))
-            visited.remove((r, c))
-            return result
-        for r in range(ROW):
-            for c in range(COL):
-                if dfs(r, c, 0): return True
+        def backtrack(row, col, i):
+            # base case
+            if i == len(word):
+                return True
+            if (row < 0 or col < 0 or row == ROW or col == COL
+                or (row, col) in visited or board[row][col] != word[i]):
+                return False
+            visited.add((row, col))
+            res = (backtrack(row + 1, col, i + 1) or 
+                    backtrack(row - 1, col, i + 1) or
+                    backtrack(row, col + 1, i + 1) or
+                    backtrack(row, col - 1, i + 1))
+            visited.remove((row, col))
+            return res
+          
+        for i in range(ROW):
+            for j in range(COL):
+                if backtrack(i, j, 0): 
+                    return True
         return False
-        
