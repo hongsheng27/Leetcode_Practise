e327e3a7513b74fc3df07452f022214bfcb9346b
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-06-30 23:33:20 +0800

    Time: 12 ms (86.75%), Space: 20.1 MB (85.56%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums) - 1
-        for j in range(len(nums) - 2, -1, -1):
-            if j + nums[j] >= goal:
-                goal = j
-        
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
         return goal == 0
-          
-            
