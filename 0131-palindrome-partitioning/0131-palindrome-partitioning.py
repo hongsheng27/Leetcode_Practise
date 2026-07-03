834869e7ce60b024329750ff3610d7ee70db2ebe
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-07-03 13:39:04 +0800

    Time: 41 ms (69.85%), Space: 34.3 MB (31.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,27 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        def backtrack(start, path):
-            # base case
-            if start >= len(s):
+        def isPalindrome(left, right):
+            while left <= right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+        def backtrack(l, path):
+            if l >= len(s):
                 res.append(path.copy())
                 return
-            # recursion case
-            for end in range(start, len(s)):
-                if isPalindrome(start, end):
-                    path.append(s[start: end + 1])
-                    backtrack(end + 1, path)
+            for r in range(l, len(s)):
+                if isPalindrome(l, r):
+                    path.append(s[l:r + 1])
+                    backtrack(r + 1, path)
                     path.pop()
-
-        def isPalindrome(l, r):
-            while l <= r:
-                if s[l] == s[r]:
-                    l += 1
-                    r -= 1
-                else:
-                    return False
-            return True
-
-
         backtrack(0, [])
         return res
+
+        
