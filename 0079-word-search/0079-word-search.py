f97232402c400d55b7063702cfcbf8141d09239b
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-07-16 00:16:12 +0800

    Time: 4329 ms (31.55%), Space: 19.5 MB (56.5%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,25 +1,24 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROW = len(board)
-        COL = len(board[0])
+        ROWS, COLS = len(board), len(board[0])
         visited = set()
-        def backtrack(row, col, i):
+        def backtrack(r, c, index):
             # base case
-            if i == len(word):
-                return True
-            if (row < 0 or col < 0 or row == ROW or col == COL
-                or (row, col) in visited or board[row][col] != word[i]):
+            if index == len(word): return True
+            if (r < 0 or c < 0 or r == ROWS or c == COLS
+                or (r, c) in visited or board[r][c] != word[index]):
                 return False
-            visited.add((row, col))
-            res = (backtrack(row + 1, col, i + 1) or 
-                    backtrack(row - 1, col, i + 1) or
-                    backtrack(row, col + 1, i + 1) or
-                    backtrack(row, col - 1, i + 1))
-            visited.remove((row, col))
+            # recursion case
+            visited.add((r, c))
+            res = (backtrack(r + 1, c, index + 1) or
+                   backtrack(r - 1, c, index + 1) or
+                   backtrack(r, c + 1, index + 1) or
+                   backtrack(r, c - 1, index + 1))
+            visited.remove((r, c))
             return res
-          
-        for i in range(ROW):
-            for j in range(COL):
-                if backtrack(i, j, 0): 
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if backtrack(row, col, 0):
                     return True
         return False
