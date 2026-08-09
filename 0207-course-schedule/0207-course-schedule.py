dea90cc94c79064f7ed033210796458a2cd37905
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-09 23:22:40 +0800

    Time: 9 ms (15.73%), Space: 21.3 MB (18.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = defaultdict(list)
-        for course, pre in prerequisites:
-            adj[pre].append(course)
-
+        adj = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            adj[pre].append(crs)
         visit = set()
-        safe = set()
-        def has_cycle(node):
-            if node not in adj or node in safe:
-                return False # to the end
-            if node in visit:
-                return True # cycle
-
-            visit.add(node)
-            for nei in adj[node]:
-                if has_cycle(nei): return True
-            visit.remove(node)
-            safe.add(node)
-            return False
-            
+        def dfs(crs):
+            if crs in visit: return False
+            if adj[crs] == []: return True
+            visit.add(crs)
+            for nei in adj[crs]:
+                if not dfs(nei): return False
+            visit.remove(crs)
+            adj[crs] = []
+            return True
+        
         for i in range(numCourses):
-            if has_cycle(i): 
-                return False
+            if not dfs(i): return False
         return True
-           
-            
