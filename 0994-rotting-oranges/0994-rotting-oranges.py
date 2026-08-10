3f4b3facb9efc4351f86b6590b5885d89aab842f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-10 22:39:57 +0800

    Time: 6 ms (19.05%), Space: 19.2 MB (78.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,36 +2,28 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         freshAmount = 0
-        res = 0
         visit = set()
-        queue = deque()
-        def bfs(q):
-            nonlocal freshAmount
-            print(q)
-            nei = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            count = -1
-            while q:
-                for _ in range(len(q)):
-                    row, col = q.popleft()
-                    for dr, dc in nei:
-                        nr, nc = row + dr, col + dc
-                        if (nr < 0 or nc < 0 or nr == ROWS or nc == COLS or
-                            grid[nr][nc] == 0 or grid[nr][nc] == 2 or (nr, nc) in visit): 
-                            continue
-                        freshAmount -= 1
-                        visit.add((nr, nc))
-                        q.append((nr, nc))
-                count += 1
-            return count
+        q = deque()
+        neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        minutes = 0
 
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == 1:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                    visit.add((r, c))
+                elif grid[r][c] == 1:
                     freshAmount += 1
-
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == 2 and (row, col) not in visit:
-                    queue.append((row, col))
-        res = max(res, bfs(queue))
-        return -1 if freshAmount > 0 else res
+        while q and freshAmount:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                for dr, dc in neighbors:
+                    nr, nc = row + dr, col + dc
+                    if (nr < 0 or nc < 0 or nr == ROWS or nc == COLS or
+                        grid[nr][nc] == 0 or (nr, nc) in visit): continue
+                    freshAmount -= 1
+                    visit.add((nr, nc))
+                    q.append((nr, nc))
+            minutes += 1
+        return -1 if freshAmount > 0 else minutes
+        
