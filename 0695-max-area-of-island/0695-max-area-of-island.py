2410719d797a44f6c541906c73d4766cf1712fb7
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-10 17:56:37 +0800

    Time: 23 ms (39.9%), Space: 19.5 MB (84.36%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,20 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid: return 0
         ROWS, COLS = len(grid), len(grid[0])
         visit = set()
-        maxTotal = 0
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or
-                (r, c) in visit or grid[r][c] == 0):
-                return 0
+        neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        res = 0
+        def bfs(r, c):
+            q = deque()
+            q.append((r, c))
             visit.add((r, c))
-            return (1 + 
-                    dfs(r + 1, c) +
-                    dfs(r - 1, c) +
-                    dfs(r, c + 1) +
-                    dfs(r, c - 1))
-        for row in range(ROWS):
-            for col in range(COLS):
-                total = dfs(row, col)
-                maxTotal = max(total, maxTotal)
-        return maxTotal
+            count = 1
+            while q:
+                row, col = q.popleft()
+                for dr, dc in neighbors:
+                    nr, nc = row + dr, col + dc
+                    if nr < 0 or nc < 0 or nr == ROWS or nc == COLS or (nr, nc) in visit or grid[nr][nc] == 0: continue
+                    visit.add((nr, nc))
+                    count += 1
+                    q.append((nr, nc))
+            return count
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1 and (i, j) not in visit:
+                    res = max(res, bfs(i, j))
+        return res
+        
