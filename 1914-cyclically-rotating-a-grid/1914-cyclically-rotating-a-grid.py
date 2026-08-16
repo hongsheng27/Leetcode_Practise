51aeca2a70575104e72c99171a0a856ae0d741b3
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-16 22:41:35 +0800

    Time: 123 ms (42.18%), Space: 19.7 MB (56.93%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.py
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.py
@@ -4,7 +4,7 @@ class Solution:
         bottom = len(grid) - 1
         left = 0
         right = len(grid[0]) - 1
-        while left <= right and top <= bottom:
+        while left < right and top < bottom:
             q = deque()
             for col in range(left, right):
                 q.append(grid[top][col])
@@ -19,20 +19,16 @@ class Solution:
                 q.append(q.popleft())
             index = 0
             for col in range(left, right + 1):
-                grid[top][col] = q[index]
-                index += 1
+                grid[top][col] = q.popleft()
             top += 1
             for row in range(top, bottom + 1):
-                grid[row][right] = q[index]
-                index += 1
+                grid[row][right] = q.popleft()
             right -= 1
             for col in range(right, left - 1, -1):
-                grid[bottom][col] = q[index]
-                index += 1
+                grid[bottom][col] = q.popleft()
             bottom -= 1
             for row in range(bottom, top - 1, -1):
-                grid[row][left] = q[index]
-                index += 1
+                grid[row][left] = q.popleft()
             left += 1
         return grid
 
