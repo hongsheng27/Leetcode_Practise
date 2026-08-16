27702d7fa54fcfcdf5ccb5bd9c7efbe11cf0f1ce
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-16 22:39:18 +0800

    Time: 114 ms (78.76%), Space: 19.8 MB (6.49%) - LeetHub

---
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.py
@@ -0,0 +1,41 @@
+class Solution:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        top = 0
+        bottom = len(grid) - 1
+        left = 0
+        right = len(grid[0]) - 1
+        while left <= right and top <= bottom:
+            q = deque()
+            for col in range(left, right):
+                q.append(grid[top][col])
+            for row in range(top, bottom):
+                q.append(grid[row][right])
+            for col in range(right, left, -1):
+                q.append(grid[bottom][col])
+            for row in range(bottom, top, -1):
+                q.append(grid[row][left])
+            k2 = k % len(q)
+            for i in range(k2):
+                q.append(q.popleft())
+            index = 0
+            for col in range(left, right + 1):
+                grid[top][col] = q[index]
+                index += 1
+            top += 1
+            for row in range(top, bottom + 1):
+                grid[row][right] = q[index]
+                index += 1
+            right -= 1
+            for col in range(right, left - 1, -1):
+                grid[bottom][col] = q[index]
+                index += 1
+            bottom -= 1
+            for row in range(bottom, top - 1, -1):
+                grid[row][left] = q[index]
+                index += 1
+            left += 1
+        return grid
+
+        
+        
+            
