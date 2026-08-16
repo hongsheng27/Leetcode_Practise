760b917db3e1ef30360397fb86d2053ffb63163a
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-17 00:29:07 +0800

    Time: 0 ms (100%), Space: 19.5 MB (7.3%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -8,16 +8,18 @@ class Solution:
             for col in range(left, right + 1): 
                 lst.append(matrix[top][col])
             top += 1
-            if top > bottom: break
+    
             for row in range(top, bottom + 1):
                 lst.append(matrix[row][right])
             right -= 1
-            if left > right: break
-            for col in range(right, left - 1, -1):
-                lst.append(matrix[bottom][col])
-            bottom -= 1
-            for row in range(bottom, top - 1, -1):
-                lst.append(matrix[row][left])
-            left += 1
+            
+            if top <= bottom:
+                for col in range(right, left - 1, -1):
+                    lst.append(matrix[bottom][col])
+                bottom -= 1
+            if left <= right:
+                for row in range(bottom, top - 1, -1):
+                    lst.append(matrix[row][left])
+                left += 1
         return lst
 
