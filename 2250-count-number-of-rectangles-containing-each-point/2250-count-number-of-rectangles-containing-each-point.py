be4c220cdcbba26702fefc6d8424c7a33d654197
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-17 21:37:29 +0800

    Time: 2051 ms (22.22%), Space: 37.9 MB (36.67%) - LeetHub

---
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
+        bucket = [[] for _ in range(101)] # keep zero
+        # O(R)
+        for l, h in rectangles:
+            bucket[h].append(l)
+        # 100 * log R
+        for h in range(1, 101):
+            bucket[h].sort()
+        # O(P)
+        res = []
+        for x, y in points:
+            arr = bucket[y:]
+            total = 0
+            for lst in arr: 
+                l = 0
+                r = len(lst)
+
+                while l < r:
+                    m = (l + r) // 2
+                    if lst[m] < x:
+                        l = m + 1
+                    elif lst[m] >= x:
+                        r = m
+                total += (len(lst) - l)
+            res.append(total)
+        return res
