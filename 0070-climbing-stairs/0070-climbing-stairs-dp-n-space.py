c03809b0255be1136298d295922f23d6278e96dc
will-neu <liu.hun@northeastern.edu>
2026-08-21 13:47:56 +0800

    feat: upload 7- n space

--- 0070-climbing-stairs/0070-climbing-stairs-dp-n-space.py
+++ 0070-climbing-stairs/0070-climbing-stairs-dp-n-space.py
@@ -2,12 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2: return n
         dp = [1, 2]
-        i = 2
-        while i < n:
-            tmp = dp[1]
-            dp[1] = dp[0] + dp[1]
-            dp[0] = tmp
-            i += 1
+
+        for i in range(3, n + 1):
+            cur = dp[0] + dp[1]
+            dp[0] = dp[1]
+            dp[1] = cur
         return dp[-1]
-
-
