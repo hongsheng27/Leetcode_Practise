170cf501d6fbe1412586bf8920a48d6c6488020e
will-neu <liu.hun@northeastern.edu>
2026-08-23 16:41:07 +0800

    feat: upload 139 bottom up v1

--- 0139-word-break/0139-word-break-bottom-up.py
+++ 0139-word-break/0139-word-break-bottom-up.py
@@ -8,7 +8,6 @@ class Solution:
             for j in range(i):
                 if dp[j] and s[j: i] in wordSet:
                     dp[i] = True
-                    break
         return dp[-1]
 
             
