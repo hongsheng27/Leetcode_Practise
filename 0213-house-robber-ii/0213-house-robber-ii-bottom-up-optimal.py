bd3635f11ad820b261199ad94c9eac4761e55aa3
will-neu <liu.hun@northeastern.edu>
2026-08-23 10:45:33 +0800

    feat: upload 213 bottom up optimal

---
+++ 0213-house-robber-ii/0213-house-robber-ii-bottom-up-optimal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(nums):
+            dp = [0, 0]
+            for n in nums:
+                tmp = dp[1]
+                dp[1] = max(dp[0] + n, dp[1])
+                dp[0] = tmp
+            return dp[1]
+        return max(nums[0], helper(nums[:-1]), helper(nums[1:]))
+        
