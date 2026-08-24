0f8bb6f5ac40a748900bfb59933a6ddad041eaa2
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-24 13:27:23 +0800

    Time: 0 ms (100%), Space: 19.3 MB (75.4%) - LeetHub

---
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numRookCaptures(self, board: List[List[str]]) -> int:
+        ROWS, COLS = len(board), len(board[0])
+        def helper(r, c):
+            count = 0
+            top = left = 0
+            bottom = len(board) - 1
+            right = len(board[0]) - 1
+            for col in range(c + 1, right + 1):
+                if board[r][col] == "B": break
+                if board[r][col] == "p": 
+                    count += 1
+                    break
+            for col in range(c - 1, left - 1, -1):
+                if board[r][col] == "B": break
+                if board[r][col] == "p": 
+                    count += 1
+                    break
+            for row in range(r + 1, bottom + 1):
+                if board[row][c] == "B": break
+                if board[row][c] == "p": 
+                    count += 1
+                    break
+            for row in range(r - 1, top - 1, -1):
+                if board[row][c] == "B": break
+                if board[row][c] == "p": 
+                    count += 1
+                    break
+            return count
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'R':
+                    return helper(r, c) 
