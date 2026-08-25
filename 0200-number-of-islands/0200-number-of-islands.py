cbfd54e876f350d2e44461d93de14c87da5e529c
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-25 11:33:15 +0800

    Time: 268 ms (19.59%), Space: 26.9 MB (17.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        visit = set() # (col, row)
-        count = 0
-
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
-                (r, c) in visit or grid[r][c] == "0"): return
-            visit.add((r, c))
-
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-       
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
+        res = 0
+        def bfs(r, c):
+            q = deque()
+            q.append((r, c))
+            visited.add((r, c))
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if (nr < 0 or nc < 0 or nr == ROWS or nc == COLS or
+                        (nr, nc) in visited or grid[nr][nc] == "0"):
+                        continue
+                    q.append((nr, nc))
+                    visited.add((nr, nc)) 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    dfs(r, c)
-                    count += 1
-        return count
-
-        
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    res += 1
+        return res
