45aefa004df51607eea71deaee8a00a5d92d3228
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-25 15:44:11 +0800

    Time: 14 ms (44.89%), Space: 24 MB (16.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        count = {}
-        for string in strs:
-            board = [0] * 26
-            for s in string:
-                board[ord(s) - ord('a')] += 1
-            board = tuple(board)
-            if board not in count:
-                count[board] = []
-            count[board].append(string)
-        return list(count.values())
-        
-        
+        count = defaultdict(list)
+        res = []
+        for s in strs:
+            alphboard = [0] * 26
+            for i in range(len(s)):
+                alphboard[ord(s[i]) - ord('a')] += 1
+            count[tuple(alphboard)].append(s)
+        for lst in count.values():
+            res.append(lst)
+        return res
