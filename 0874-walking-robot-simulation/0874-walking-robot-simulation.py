0215a207bcbbed3fd59e0b0464b4e4195b6b5e36
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-25 13:21:06 +0800

    Time: 97 ms (94.28%), Space: 23.8 MB (69.73%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.py
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -1,31 +1,13 @@
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
-        start = (0, 0)
-        d = 0 # North
-        maxDistance = 0
+        x = y = d = maxDistance = 0
         directions = [
             (0, 1), # North
             (1, 0), # East
             (0, -1), # South
             (-1, 0) # West
         ]
-     
-        obstacleSet = set()
-        for x, y in obstacles:
-            obstacleSet.add((x, y))
-
-        def walk(x, y, d, step):
-            nx = x
-            ny = y
-            while step:
-                x = x + directions[d][0]
-                y = y + directions[d][1]
-                if (x, y) in obstacleSet:
-                    return (nx, ny)
-                nx = x
-                ny = y
-                step -= 1
-            return (nx, ny)
+        blocked = {(x, y) for x, y in obstacles}
         
         for command in commands:
             if command == -1: 
@@ -33,8 +15,13 @@ class Solution:
             elif command == -2:
                 d = (d - 1) % 4
             else:
-                start = walk(start[0], start[1], d, command)
-                maxDistance = max(maxDistance, start[0]**2 + start[1]**2)
+                dx, dy = directions[d]
+                for _ in range(command):
+                    nx, ny = x + dx, y + dy
+                    if (nx, ny) in blocked:
+                        break
+                    x, y = nx, ny
+                maxDistance = max(maxDistance, x ** 2 + y ** 2)
         return maxDistance
 
 
