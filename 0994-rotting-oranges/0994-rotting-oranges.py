344ec46ec422cac39451045e99ca0fd1110f708f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-26 12:10:14 +0800

    Time: 5 ms (22.87%), Space: 19.6 MB (12.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,29 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        freshAmount = 0
-        visit = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
         q = deque()
-        neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        minutes = 0
-
+        freshAmount = res = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     q.append((r, c))
-                    visit.add((r, c))
-                elif grid[r][c] == 1:
+                    visited.add((r, c))
+                if grid[r][c] == 1:
                     freshAmount += 1
-        while q and freshAmount:
+        while q:
             for _ in range(len(q)):
                 row, col = q.popleft()
-                for dr, dc in neighbors:
+                for dr, dc in directions:
                     nr, nc = row + dr, col + dc
                     if (nr < 0 or nc < 0 or nr == ROWS or nc == COLS or
-                        grid[nr][nc] == 0 or (nr, nc) in visit): continue
-                    freshAmount -= 1
-                    visit.add((nr, nc))
+                        (nr, nc) in visited or grid[nr][nc] != 1): continue
                     q.append((nr, nc))
-            minutes += 1
-        return -1 if freshAmount > 0 else minutes
-        
+                    visited.add((nr, nc))
+                    freshAmount -= 1
+            if q: res += 1 
+        return -1 if freshAmount > 0 else res
+
+
