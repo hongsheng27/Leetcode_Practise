f8174d2bdfcb5079acd5f7958f3a840174f980a4
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-08-28 12:33:19 +0800

    Time: 1829 ms (32%), Space: 37.5 MB (78%) - LeetHub

--- 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.py
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.py
@@ -1,22 +1,28 @@
 class Solution:
     def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
-        buckets = [[] for _ in range(101)]
-        for x, y in rectangles:
-            buckets[y].append(x)
-        for bucket in buckets:
-            bucket.sort()
+        bucket = [[] for _ in range(101)] # keep zero
+        # O(R)
+        for l, h in rectangles:
+            bucket[h].append(l)
+        # O(R log R)
+        for h in range(1, 101):
+            bucket[h].sort()
+        # O(P log R)
         res = []
         for x, y in points:
+            arr = bucket[y:]
             total = 0
-            for bucket in buckets[y:]:
+            for lst in arr: 
                 l = 0
-                r = len(bucket)
+                r = len(lst)
+
                 while l < r:
                     m = (l + r) // 2
-                    if bucket[m] < x:
+                    if lst[m] < x:
                         l = m + 1
                     else:
                         r = m
-                total += len(bucket) - l
+                total += (len(lst) - l)
             res.append(total)
+        # O(R) + O(R log R) + O(P log R) = O(R log R + P log R)
         return res
