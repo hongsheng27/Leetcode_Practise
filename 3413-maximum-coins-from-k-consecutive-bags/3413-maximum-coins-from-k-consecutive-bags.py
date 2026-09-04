1cd547524412fedd9707c093ff86f6417dfaa4d1
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-03 20:30:56 -0700

    Time: 579 ms (83.86%), Space: 70.8 MB (58.22%) - LeetHub

--- 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.py
+++ 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.py
@@ -1,31 +1,28 @@
 class Solution:
     def maximumCoins(self, coins: List[List[int]], k: int) -> int:
         coins.sort()
-        res = 0
-        n = len(coins)
-
+        maxMoney = 0
         left = bag = 0
-        for right in range(n):
-            l, r, c = coins[right]
-            bag += (r - l + 1) * c
+        for right in range(len(coins)):
+            l, r, coin = coins[right]
             windowStart = r - k + 1
+            bag += (r - l + 1) * coin
 
             while left < right and coins[left][1] < windowStart:
-                x, y, coin = coins[left]
-                bag -= (y - x + 1) * coin
-                left += 1 
+                x, y, c = coins[left]
+                bag -= (y - x + 1) * c
+                left += 1
             current = bag
-            
             if left <= right:
                 x, y, c = coins[left]
                 if x < windowStart:
                     outsideLength = windowStart - x
                     current -= outsideLength * c
-            res = max(res, current)
-
-        right = n - 1
+            maxMoney = max(maxMoney, current)
+        
+        right = len(coins) - 1
         bag = 0
-        for left in range(n - 1, -1, -1):
+        for left in range(len(coins) - 1, -1, -1):
             l, r, coin = coins[left]
             bag += (r - l + 1) * coin
             windowEnd = l + k - 1
@@ -37,11 +34,9 @@ class Solution:
             current = bag
             if left <= right:
                 x, y, c = coins[right]
-                if y >= windowEnd:
+                if y > windowEnd:
                     outsideLength = y - windowEnd
                     current -= outsideLength * c
-            res = max(res, current)
-        return res
-
-
+            maxMoney = max(maxMoney, current)
+        return maxMoney
             
