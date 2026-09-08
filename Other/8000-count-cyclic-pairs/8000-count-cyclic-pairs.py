83f4f2b83e444b505365fe8c97a525e9dbf092e2
will-neu <liu.hun@northeastern.edu>
2026-09-08 16:01:36 -0700

    feat: uplaod 8000

--- Other/8000-count-cyclic-pairs/8000-count-cyclic-pairs.py
+++ Other/8000-count-cyclic-pairs/8000-count-cyclic-pairs.py
@@ -1,15 +1,18 @@
-def countCyclicPairs(nums):
-  buckets = defaultdict(list)
-  res = 0
-  for num in nums:
-    s = str(num)
-    key = s
-    doubled = s + s
-    for i in range(len(s)):
-      key = min(key, doubled[i: i + len(s)])
-    buckets[key].append(num)
+from collections import defaultdict
 
-  for bucket in buckets.values():
-    k = len(bucket)
-    res += k * (k - 1) // 2
-  return res
+class Solution:
+    def countCyclicPairs(self, nums):
+      group = defaultdict(int)
+      for n in nums:
+        s = str(n)
+        doubled = s + s
+        key = s
+        for j in range(len(s)):
+          rotation = doubled[j: j + len(s)]
+          key = min(key, rotation)
+        group[key] += 1
+      res = 0
+      for count in group.values():
+        res += count * (count - 1) // 2
+      return res
+        
