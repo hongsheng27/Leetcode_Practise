af2e73ed2d2a7d8df91187eeb21f9235897191ea
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-08 11:39:53 -0700

    Time: 3 ms (75.35%), Space: 19.4 MB (13.3%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,9 +2,10 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        visited = set()
         q = deque()
-        freshAmount = res = 0
+        visited = set()
+        freshAmount = 0
+
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
@@ -12,17 +13,16 @@ class Solution:
                     visited.add((r, c))
                 if grid[r][c] == 1:
                     freshAmount += 1
-        while q and freshAmount > 0:
+        phrase = 0
+        while q and freshAmount:
             for _ in range(len(q)):
                 row, col = q.popleft()
                 for dr, dc in directions:
                     nr, nc = row + dr, col + dc
-                    if (nr < 0 or nc < 0 or nr == ROWS or nc == COLS or
+                    if (nr < 0 or nc < 0 or nr == ROWS or nc == COLS or 
                         (nr, nc) in visited or grid[nr][nc] != 1): continue
                     q.append((nr, nc))
                     visited.add((nr, nc))
                     freshAmount -= 1
-            res += 1 
-        return -1 if freshAmount > 0 else res
-
-
+            phrase += 1
+        return -1 if freshAmount > 0 else phrase
