1b359e28a947456a197afe37134c5df52cd61604
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-10 22:41:21 -0700

    Time: 7 ms (48.06%), Space: 20.3 MB (77.14%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -3,10 +3,8 @@ class Solution:
         parent = [i for i in range(n)]
         size = [1] * n
         res = n
-
         def find(x):
             while x != parent[x]:
-                parent[x] = parent[parent[x]]
                 x = parent[x]
             return x
         def union(x, y):
@@ -14,17 +12,14 @@ class Solution:
             rootX = find(x)
             rootY = find(y)
 
-            if rootX == rootY:
-                return False
-            
+            if rootX == rootY: return False
+
             if size[rootX] < size[rootY]:
                 rootX, rootY = rootY, rootX
-            
             parent[rootY] = rootX
             size[rootX] += size[rootY]
             res -= 1
-           
+
         for x, y in edges:
             union(x, y)
-
         return res
