b16abe5c007c60e95d46c10d2f5641b4f38e17e5
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-14 15:22:29 -0700

    Time: 43 ms (10.77%), Space: 32.1 MB (6.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-  
-        def dfs(r, c, cache):
+        cache = {}
+        def dfs(r, c):
             if r == ROWS or c == COLS: return float('inf')
             if (r, c) in cache: return cache[(r, c)]
             if r == ROWS - 1 and c == COLS - 1: return grid[ROWS - 1][COLS - 1]
             
-            cache[(r, c)] = grid[r][c] + min(dfs(r + 1, c, cache), dfs(r, c + 1, cache))
+            cache[(r, c)] = grid[r][c] + min(dfs(r + 1, c), dfs(r, c + 1))
             return cache[(r, c)]
-        return dfs(0, 0, {})
+        return dfs(0, 0)
 
         
