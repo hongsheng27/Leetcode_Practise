e07f831699406faa3a49c9570b7d5fb304a13039
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-16 17:17:19 -0700

    Time: 1031 ms (9.12%), Space: 20.5 MB (52.3%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,28 @@
+class Solution:
+    def openLock(self, deadends: list[str], target: str) -> int:
+        if "0000" in deadends: return -1
+        if "0000" in target: return 0
+        start = (0, 0, 0, 0)
+        directions = [(1, 0, 0, 0), (-1, 0, 0, 0), (0, 1, 0, 0), (0, -1, 0, 0), (0, 0, 1, 0),  (0, 0, -1, 0),  (0, 0, 0, 1), (0, 0, 0, -1)]
+        q = deque([start])
+        visited = set([start])
+        res = 0
+        findTarget = False
+        while q and not findTarget:
+            for _ in range(len(q)):
+                f, s, t, fo = q.popleft()
+                for df, ds, dt, dfo in directions:
+                    nf, ns, nt, nfo = self.transfer(f + df), self.transfer(s + ds), self.transfer(t + dt), self.transfer(fo + dfo)
+                    cur = str(nf) + str(ns) + str(nt) + str(nfo)
+                    if cur in visited: continue
+                    if cur in deadends: continue
+                    if cur == target: findTarget = True
+                    visited.add(cur)
+                    q.append((nf, ns, nt, nfo))
+            res += 1
+        return res if findTarget else -1
+                
+    def transfer(self, number):
+        number = 0 if number == 10 else number
+        number = 9 if number == -1 else number
+        return number
