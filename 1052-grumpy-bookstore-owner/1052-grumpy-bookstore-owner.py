d4f9e366eef16967b74835af8a64ad524394b23f
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-17 21:51:50 -0700

    Time: 12 ms (80.51%), Space: 21.9 MB (57.13%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,26 +1,24 @@
 class Solution:
     def maxSatisfied(self, customers: list[int], grumpy: list[int], minutes: int) -> int:
         n = len(customers)
-        changeWindow = 0
+        base = 0
+        extra = 0
+
+        for i in range(n):
+            if grumpy[i] == 0:
+                base += customers[i]
         for i in range(minutes):
             if grumpy[i] == 1:
-                changeWindow += customers[i]
-        l = 0
-        maxChange = (0, minutes - 1, changeWindow) # (l, r, sum)
-        for l in range(1, n - minutes + 1):
-            r = l + minutes - 1
+                extra += customers[i]
+        maxExtra = extra
+
+        for r in range(minutes, n):
+            l = r - minutes + 1
             if grumpy[r] == 1:
-                changeWindow += customers[r]
+                extra += customers[r]
             if grumpy[l - 1] == 1:
-                changeWindow -= customers[l - 1]
+                extra -= customers[l - 1]
+            maxExtra = max(maxExtra, extra)
+        return base + maxExtra
+            
 
-            if changeWindow > maxChange[2]:
-                maxChange = (l, r, changeWindow)
-
-        for i in range(maxChange[0], maxChange[1] + 1):
-            grumpy[i] = 0
-        res = 0
-        for i in range(len(customers)):
-            if grumpy[i] == 0:
-                res += customers[i]
-        return res
