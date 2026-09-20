229b86d7f663830833ea8e9539816862c0b61bcd
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-20 00:50:31 -0700

    Time: 477 ms (21.5%), Space: 20.6 MB (26.1%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,27 +1,30 @@
 class Solution:
     def openLock(self, deadends: list[str], target: str) -> int:
         if "0000" in deadends: return -1
-        if target == "0000": return 0
-        directions = [(1, 0, 0, 0), (-1, 0, 0, 0), 
-                      (0, 1, 0, 0), (0, -1, 0, 0),
-                       (0, 0, 1, 0), (0, 0, -1, 0),
-                       (0, 0, 0, 1), (0, 0, 0, -1)]
+        if target == '0000': return 0
         deadends = set(deadends)
-        q = deque([(0, 0, 0, 0)])
-        visited = set()
-        move = 0
+        start = (0, 0, 0, 0)
+        directions = [(1, 0, 0, 0), (-1, 0, 0, 0), 
+                      (0, 1, 0, 0), (0, -1, 0, 0), 
+                      (0, 0, 1, 0),  (0, 0, -1, 0),  
+                      (0, 0, 0, 1), (0, 0, 0, -1)]
+        q = deque([start])
+        visited = {"0000"}
+        res = 0
         while q:
             for _ in range(len(q)):
                 f, s, t, fo = q.popleft()
                 for df, ds, dt, dfo in directions:
                     nf, ns, nt, nfo = self.transfer(f + df), self.transfer(s + ds), self.transfer(t + dt), self.transfer(fo + dfo)
-                    cur = str(nf) + str(ns) + str(nt) + str(nfo)
-                    if cur == target: return move + 1
-                    if cur in deadends or cur in visited: continue
-                    q.append((nf, ns, nt, nfo))
+                    cur = f"{nf}{ns}{nt}{nfo}"
+                    if cur in visited or cur in deadends: continue
+                    if cur == target: 
+                        return res + 1
                     visited.add(cur)
-            move += 1
+                    q.append((nf, ns, nt, nfo))
+            res += 1
         return -1
+                
     def transfer(self, number):
         number = 0 if number == 10 else number
         number = 9 if number == -1 else number
