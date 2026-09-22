3732a855ff20bdfd6830281a2f187424001f8ef7
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-21 20:21:22 -0700

    Time: 103 ms (62.26%), Space: 24.5 MB (59.14%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -4,11 +4,11 @@ class Solution:
         n = len(nums)
         nums = nums + nums
         windowSum = 0
-        res = float('inf')
+    
         l = 0
         for i in range(amount):
             windowSum += nums[i]
-        res = min(res, amount - windowSum)
+        res = amount - windowSum
         for l in range(1, n):
             r = l + amount - 1
             windowSum += nums[r] 
