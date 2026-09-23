327be6d646ef0c1dbd24531bfdf4d17d30496efe
hungsheng liu <145427728+hongsheng27@users.noreply.github.com>
2026-09-22 21:20:13 -0700

    Time: 5 ms (69.04%), Space: 21.8 MB (9.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,18 +4,14 @@ class Solution:
         dominantCount = 0
         for n in nums:
             if dominant == None:
-                print('-1-')
                 dominant = n
                 dominantCount += 1
             elif dominant != n:
                 dominantCount -= 1
-                print('-2-')
                 if dominantCount == 0:
                     dominant = None
                     dominantCount = 0
             else:
-                print('-3-')
                 dominantCount += 1
-            print(dominant, dominantCount)
         return dominant
                 
